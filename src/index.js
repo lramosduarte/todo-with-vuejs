bd318f4402e533cd32e636f93cb92eb518df0686
lramosduarte@gmail.com <Leonardo Ramos Duarte>
2017-10-19 15:58:49 -0200

    Implementa adicona tarefa

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import bar from './extras/bar';
+import Vue from 'vue';
 
-bar();
+let apps = new Vue({
+    el: '#app',
+    data: {
+        descricaoTarefa: '',
+        listaTarefas: []
+    },
+    methods: {
+        adicionaTarefa: function() {
+            let novaTarefa = {
+                descricao: this.descricaoTarefa,
+                id: String(Math.random()).substring(2,5)
+            };
+            this.listaTarefas.push(novaTarefa)
+            this.descricaoTarefa = ''
+        }
+    }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 module.exports = {
     entry: './src/index.js',
+    devServer: {
+        contentBase: './app'
+    },
     output: {
         filename: './app/bundle.js'
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.js'
+        }
     }
 }
